d42a716e91edfe089b36efb19e70d447e77b3746
Danil <danilogomanjuk93@gmail.com>
2024-12-12 11:53:59 +0100

    First commit

---
+++ src/main/java/com/softuniProject/dataBaseRestoranTableManagment/DataBaseRestoranTableManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.softuniProject.dataBaseRestoranTableManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseRestoranTableManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseRestoranTableManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softuniProject/dataBaseRestoranTableManagment/DataBaseRestoranTableManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuniProject.dataBaseRestoranTableManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseRestoranTableManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
